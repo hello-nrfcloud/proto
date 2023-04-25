2372e42167624218282772af647921533737d94d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-04-25 12:47:38 +0200

    fix: include schemas for typescript

--- compile.sh
+++ compile.sh
@@ -6,4 +6,5 @@ set -x
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/nrfCloud ./nrfCloud
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/nrfGuide ./nrfGuide
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/validator ./validator
+npx swc -C jsc.experimental.keepImportAssertions=true -d dist/generate ./generate
 cp -r schemas ./dist
