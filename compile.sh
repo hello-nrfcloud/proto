735f8e9336ac9606e30695d8dc74ee672469a49e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-22 13:06:50 +0200

    fix: compile fingerprint source

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ npx swc -C jsc.experimental.keepImportAssertions=true -d dist/nrfCloud ./nrfClou
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/hello ./hello
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/validator ./validator
 npx swc -C jsc.experimental.keepImportAssertions=true -d dist/generate ./generate
+npx swc -C jsc.experimental.keepImportAssertions=true -d dist/fingerprint ./fingerprint
 cp -r ./schemas ./dist
 mkdir ./dist/nrfcloud-application-protocols
 cp -r ./nrfcloud-application-protocols/schemas ./dist/nrfcloud-application-protocols
