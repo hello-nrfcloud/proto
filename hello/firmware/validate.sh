3a1fda50d031444442de7f03b34c1e8acd92e18a
Markus Tacker <m@coderbyheart.com>
2023-07-07 11:11:19 +0200

    ci: print JSON representation

--- hello/firmware/validate.sh
+++ hello/firmware/validate.sh
@@ -8,6 +8,9 @@ D2C_DIR="${TEMPDIR}/deviceToCloud"
 mkdir $D2C_DIR
 for file in deviceToCloud/*.json; do
   filename=$(basename "$file" .json)
+  echo ""
+  echo "# deviceToCloud: ${filename}"
+  cat $file | jq
   echo "converting $filename"
   zcbor convert -c deviceToCloud.cddl -i $file -o $D2C_DIR/$filename.cbor --input-as json --output-as cbor -t deviceToCloud-message
   echo "comparing $filename"
@@ -18,6 +21,9 @@ C2D_DIR="${TEMPDIR}/cloudToDevice"
 mkdir -p $C2D_DIR
 for file in cloudToDevice/*.json; do
   filename=$(basename "$file" .json)
+  echo ""
+  echo "# cloudToDevice: ${filename}"
+  cat $file | jq
   echo "converting $filename"
   zcbor convert -c cloudToDevice.cddl -i $file -o $C2D_DIR/$filename.cbor --input-as json --output-as cbor -t cloudToDevice-message
   echo "comparing $filename"
