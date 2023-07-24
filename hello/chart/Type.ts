1281e78051fc6409ea5af9a2d388f61dfa7b4b5c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-07-24 12:15:42 +0200

    feat(history): add week chart

--- hello/chart/Type.ts
+++ hello/chart/Type.ts
@@ -4,5 +4,5 @@ import { Type } from '@sinclair/typebox'
  * Defines the chart types provided by the hello.nrfcloud.com backend.
  */
 export const ChartType = Type.Union(
-	['lastHour', 'lastDay', 'lastMonth'].map((s) => Type.Literal(s)),
+	['lastHour', 'lastDay', 'lastWeek', 'lastMonth'].map((s) => Type.Literal(s)),
 )
