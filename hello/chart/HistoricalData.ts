0807f2933e3dc274885d5dad74d140dfcf7901f5
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-07-24 11:01:10 +0200

    fix(history): backend returns floats

--- hello/chart/HistoricalData.ts
+++ hello/chart/HistoricalData.ts
@@ -1,9 +1,18 @@
 import { Type } from '@sinclair/typebox'
 import { Gain } from '../Gain.js'
 import { Location } from '../Location.js'
-import { Battery } from '../Battery.js'
+import { ts } from 'hello/ts.js'
 
-export const BatteryData = Type.Pick(Battery, ['%', 'ts'])
+export const BatteryData = Type.Object({
+	ts,
+	// History contains averages, which are floats
+	'%': Type.Number({
+		minimum: 0,
+		maximum: 100,
+		examples: [94.5],
+		description: 'Battery capacity in percent',
+	}),
+})
 
 export const GainData = Type.Pick(Gain, ['mA', 'ts'])
 
