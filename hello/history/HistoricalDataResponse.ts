25b745721b3a29681cfee9bd1d831b242486a235
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-08-09 23:40:34 +0200

    feat(history): add common response types

--- hello/history/HistoricalDataResponse.ts
+++ hello/history/HistoricalDataResponse.ts
@@ -13,6 +13,7 @@ import {
 	locationMessage,
 	locationTrailMessage,
 } from './HistoricalDataRequest.js'
+import { ts } from '../ts.js'
 
 export const CommonResponse = Type.Object({
 	'@context': Type.Literal(Context.historicalDataResponse.toString()),
@@ -21,8 +22,22 @@ export const CommonResponse = Type.Object({
 	type: TimeSpan,
 })
 
-export const BatteryResponse = Type.Composite([
+export const CommonAggregatedResponse = Type.Composite([
 	CommonResponse,
+	Type.Object({
+		attributes: Type.Record(Type.String(), Type.Array(Type.Object({ ts }))),
+	}),
+])
+
+export const CommonUnaggregatedResponse = Type.Composite([
+	CommonResponse,
+	Type.Object({
+		attributes: Type.Array(Type.Object({ ts })),
+	}),
+])
+
+export const BatteryResponse = Type.Composite([
+	CommonAggregatedResponse,
 	Type.Object({
 		message: batteryMessage,
 		attributes: Type.Record(Type.String(), Type.Array(BatteryData)),
@@ -30,7 +45,7 @@ export const BatteryResponse = Type.Composite([
 ])
 
 export const GainResponse = Type.Composite([
-	CommonResponse,
+	CommonAggregatedResponse,
 	Type.Object({
 		message: gainMessage,
 		attributes: Type.Record(Type.String(), Type.Array(GainData)),
@@ -38,7 +53,7 @@ export const GainResponse = Type.Composite([
 ])
 
 export const LocationResponse = Type.Composite([
-	CommonResponse,
+	CommonUnaggregatedResponse,
 	Type.Object({
 		message: locationMessage,
 		attributes: Type.Array(LocationData),
@@ -46,7 +61,7 @@ export const LocationResponse = Type.Composite([
 ])
 
 export const LocationTrailResponse = Type.Composite([
-	CommonResponse,
+	CommonUnaggregatedResponse,
 	Type.Object({
 		attributes: Type.Array(LocationTrailData),
 		message: locationTrailMessage,
