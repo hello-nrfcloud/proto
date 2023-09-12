dae19b5b910d939c62fdda6332ef37927a554e01
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-09-12 15:00:21 +0200

    fix: emit assert for import assertions
    Use the new SWC configuration settings added in https://github.com/swc-project/swc/pull/7914

--- compile.sh
+++ compile.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 
-npx swc -C jsc.experimental.keepImportAssertions=true -d dist/nrfCloud ./nrfCloud
-npx swc -C jsc.experimental.keepImportAssertions=true -d dist/hello ./hello
-npx swc -C jsc.experimental.keepImportAssertions=true -d dist/validator ./validator
-npx swc -C jsc.experimental.keepImportAssertions=true -d dist/generate ./generate
-npx swc -C jsc.experimental.keepImportAssertions=true -d dist/fingerprint ./fingerprint
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true-d dist/nrfCloud ./nrfCloud
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/hello ./hello
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/validator ./validator
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/generate ./generate
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/fingerprint ./fingerprint
 cp -r ./schemas ./dist
 mkdir ./dist/nrfcloud-application-protocols
 cp -r ./nrfcloud-application-protocols/schemas ./dist/nrfcloud-application-protocols
