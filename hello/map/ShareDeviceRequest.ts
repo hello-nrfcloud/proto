178f11b00bb5d281b9d2d554dea370288d35baa3
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-17 17:45:37 +0100

    fix: resolve conflicting star export

--- hello/map/ShareDeviceRequest.ts
+++ hello/map/ShareDeviceRequest.ts
@@ -2,7 +2,7 @@ import { Type } from '@sinclair/typebox'
 import { Context } from '../Context.js'
 import { PublicDeviceId } from './DeviceId.js'
 
-export const ShareDeviceOwnershipConfirmed = Type.Object({
+export const ShareDeviceRequest = Type.Object({
 	'@context': Type.Literal(Context.map.shareDevice.request.toString()),
 	id: PublicDeviceId,
 })
