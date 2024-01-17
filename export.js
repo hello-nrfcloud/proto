4dbba6d5dabd90a81c1e958f023d952ee3100d18
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-17 14:17:48 +0100

    fix: update export

--- export.js
+++ export.js
@@ -1,4 +1,4 @@
 export * from './dist/validator/validateWithJSONSchema.js'
 export * from './dist/validator/validateWithTypeBox.js'
 export * from './dist/validator/formatTypeBoxErrors.js'
-export * from './dist/validator/getShadowUpdateTime.js'
+export * from './dist/nrfCloud/getShadowUpdateTime.js'
