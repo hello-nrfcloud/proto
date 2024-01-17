717d1642acbd686f1fe505bf1ec0a470836a648a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-17 14:08:48 +0100

    feat(validation): add formatTypeBoxErrors

--- export.js
+++ export.js
@@ -1,2 +1,4 @@
 export * from './dist/validator/validateWithJSONSchema.js'
 export * from './dist/validator/validateWithTypeBox.js'
+export * from './dist/validator/formatTypeBoxErrors.js'
+export * from './dist/validator/getShadowUpdateTime.js'
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export * from './nrfCloud/getShadowUpdateTime.js'
 export * from './validator/validateWithJSONSchema.js'
 export * from './validator/validateWithTypeBox.js'
+export * from './validator/formatTypeBoxErrors.js'
