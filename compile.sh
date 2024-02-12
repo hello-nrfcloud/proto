d7dada1d2608dec86e5b446f8ffa1dcdd93668cb
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-12 16:45:17 +0100

    fix: SWC compile changed behaviour

--- compile.sh
+++ compile.sh
@@ -3,11 +3,12 @@
 set -e
 set -x
 
-npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/nrfCloud ./nrfCloud
-npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/hello ./hello
-npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/validator ./validator
-npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/generate ./generate
-npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/fingerprint ./fingerprint
+rm -rf dist/
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/ ./nrfCloud
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/ ./hello
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/ ./validator
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/ ./generate
+npx swc -C jsc.experimental.keepImportAttributes=true -C jsc.experimental.emitAssertForImportAttributes=true -d dist/ ./fingerprint
 cp -r ./schemas ./dist
 mkdir ./dist/nrfcloud-application-protocols
 cp -r ./nrfcloud-application-protocols/schemas ./dist/nrfcloud-application-protocols
