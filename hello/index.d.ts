d8d7cb7f7802c75fd01e0519ed6293bea7cb9b3f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-18 17:02:17 +0100

    fix: export StatusCode again

--- hello/index.d.ts
+++ hello/index.d.ts
@@ -1,6 +1,7 @@
 export * from './Context.js'
 export * from './convert.js'
 export * from './errors/ProblemDetail.js'
+export * from './errors/StatusCode.js'
 export * from './ConfigureDevice.js'
 export * from './SingleCellGeoLocation.js'
 export * from './incomingMessageValidator.js'
