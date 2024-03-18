6a3c7937fda04810cb6c3b68b16445427fd9994e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-18 17:08:16 +0100

    fix(types): use correct export

--- export.js
+++ export.js
@@ -1,4 +1,3 @@
 export * from './dist/validator/validateWithJSONSchema.js'
 export * from './dist/validator/validateWithTypeBox.js'
 export * from './dist/validator/formatTypeBoxErrors.js'
-export * from './dist/nrfCloud/getShadowUpdateTime.js'
--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
-export * from './nrfCloud/getShadowUpdateTime.js'
 export * from './validator/validateWithJSONSchema.js'
 export * from './validator/validateWithTypeBox.js'
 export * from './validator/formatTypeBoxErrors.js'
