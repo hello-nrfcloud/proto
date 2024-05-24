ccdde219e86b8cc5004bff33a1db658f1a34ed25
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-05-24 23:24:34 +0200

    refactor: removed
    This should be implemented in the host project
    
    BREAKING CHANGE: this removes incomingMessageValidator

--- hello/errors/examples.spec.ts
+++ hello/errors/examples.spec.ts
@@ -1,10 +1,10 @@
 import { validateWithTypeBox } from '../../validator/validateWithTypeBox.js'
 import { ProblemDetail } from './ProblemDetail.js'
-import BAD_REQUEST from './examples/BAD_REQUEST.json' assert { type: 'json' }
-import INTERNAL_ERROR from './examples/INTERNAL_ERROR.json' assert { type: 'json' }
-
+import BAD_REQUEST from './examples/BAD_REQUEST.json'
+import INTERNAL_ERROR from './examples/INTERNAL_ERROR.json'
 import { describe, test as it } from 'node:test'
 import assert from 'node:assert/strict'
+
 void describe('Error example messages', () => {
 	const validator = validateWithTypeBox(ProblemDetail)
 
