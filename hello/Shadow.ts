7b0f972deb879ea2804bac00d0e2e387449e06b0
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-05-25 01:32:46 +0200

    fix(shadow): remove version and update ts

--- hello/Shadow.ts
+++ hello/Shadow.ts
@@ -1,5 +1,4 @@
 import { Type, type Static } from '@sinclair/typebox'
-import { IsoDateType } from './IsoDateType.js'
 import { Context } from './Context.js'
 import { LwM2MObjectInstance } from '@hello.nrfcloud.com/proto-map/api'
 
@@ -7,14 +6,6 @@ export const Shadow = Type.Object({
 	'@context': Type.Literal(Context.shadow.toString()),
 	reported: Type.Array(LwM2MObjectInstance),
 	desired: Type.Array(LwM2MObjectInstance),
-	version: Type.Number({
-		minimum: 1,
-		title: 'Version',
-		description: 'The version of the shadow document.',
-	}),
-	updatedAt: IsoDateType(
-		'The timestamp when the shadow document was last updated formatted as an ISO 8601 string',
-	),
 })
 
 export type ShadowType = Static<typeof Shadow>
