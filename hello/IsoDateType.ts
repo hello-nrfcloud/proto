fd93b59f0d9f20881211711f84163772e5299089
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-05-27 11:52:31 +0200

    fix(hello): improve IsoDateType type

--- hello/IsoDateType.ts
+++ hello/IsoDateType.ts
@@ -1,6 +1,6 @@
-import { Type, type TSchema } from '@sinclair/typebox'
+import { Type, type TString } from '@sinclair/typebox'
 
-export const IsoDateType = (description?: string): TSchema =>
+export const IsoDateType = (description?: string): TString =>
 	Type.String({
 		pattern:
 			'^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2}(?:.[0-9]*)?)((-([0-9]{2}):([0-9]{2})|Z)?)$',
