262c4887244bb0120837f4c5cbf3373eb68da19b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-06 17:58:25 +0200

    fix(hello): history now has a timestamp propery

--- hello/LwM2MObjectHistory.ts
+++ hello/LwM2MObjectHistory.ts
@@ -7,13 +7,25 @@ import {
 	Resources,
 } from '@hello.nrfcloud.com/proto-map/api'
 import { deviceId } from './deviceId.js'
+import { IsoDateType } from './IsoDateType.js'
 
 export const LwM2MObjectHistory = Type.Object(
 	{
 		'@context': Type.Literal(Context.lwm2mObjectHistory.toString()),
-		partialInstances: Type.Array(Resources, {
-			title: 'The resources of the object instance.',
-		}),
+		partialInstances: Type.Array(
+			Type.Union([
+				Resources,
+				Type.Object({
+					ts: IsoDateType(
+						'Timestamp when the object was written, or in case of aggregates the beginning of the binned time span.',
+					),
+				}),
+			]),
+			{
+				title:
+					'The resources of the object instance. Does not include the Time resource.',
+			},
+		),
 		query: Type.Object(
 			{
 				ObjectID,
